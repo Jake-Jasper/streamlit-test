961bf605877b0e3456d1c4a8d01b6c81bbf51646
Jake <35571955+Jake-Jasper@users.noreply.github.com>
2022-04-20 19:18:42 +0100

    Update testApp.py

--- testApp.py
+++ testApp.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plt
 
 st.title("Normal distribution")
 
-samples = st.slider("samples", 0,10_000)
-mu = st.slider("mu",0.0,1.0,step=0.001)
-sigma = st.slider("sigma",0.0,1.0, step=0.001)
+samples = st.slider("samples", 0,10_000, value=100)
+mu = st.slider("mu",0.0,1.0,step=0.001, value=0.5)
+sigma = st.slider("sigma",0.0,1.0, step=0.001, value=0.1)
 sample = np.random.normal(mu, sigma, samples)
 bins = st.slider("bins",1,1000, value=10)
 
